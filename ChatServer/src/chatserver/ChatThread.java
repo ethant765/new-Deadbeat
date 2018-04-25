ba0d0015bba8c42b2b982a4ed264c959bdf8d6e0
ethant765 <32670682+ethant765@users.noreply.github.com>
2018-04-25 03:40:58 +0100

    updated files

--- ChatServer/src/chatserver/ChatThread.java
+++ ChatServer/src/chatserver/ChatThread.java
@@ -8,6 +8,9 @@ import java.net.*;
 import java.sql.ResultSet;
 import net.deadbeat.utility.Log;
 import java.sql.*;
+import net.deadbeat.json.JSONAdapter;
+import net.deadbeat.json.JSONProperty;
+import net.deadbeat.utility.Tokenizer;
         
 /**
  *
@@ -24,6 +27,7 @@ public class ChatThread implements Runnable {
     InetAddress userIP;
     int userPort;
     int clientID;
+    boolean opSuccess;
     
     //constructor
     ChatThread(DatagramSocket sok, DatagramPacket pak){
@@ -44,9 +48,67 @@ public class ChatThread implements Runnable {
                 clientID = IDresult.getInt("User_ID");
             }
         }catch(Exception e){Log.Throw(e);}
+        
+        //default opSuccess to true/successfull
+        //then if error caught change boolean later
+        opSuccess = true;
+    }
+    
+    public ChatThread(dataHeaders headers){
+        this.headers = headers;
     }
     
     public void run(){
+        //get the data sent from the user to use in this thread
+        byte[] data = new byte[1024];
+        data = packet.getData();
+        String Message = new String(data);
+        
+        //create a JSON object to convert the sent string into the initial data sent by the user
+        JSONAdapter jAdapter = new JSONAdapter();
+        jAdapter.fromString(Message);
+        String headerInfo = jAdapter.get("HEADER").get();
+        
+        //create a JSON object used for storing and sending return information back to the client
+        JSONAdapter sendBackData = new JSONAdapter();
+        
+        if(String.valueOf(headers.RECEIVE_MESSAGES).equals(headerInfo)) sendBackData.fromResultSet(receiveMessages());
+        else if(String.valueOf(headers.SEND_MESSAGE).equals(headerInfo)) sendMessage(jAdapter);
+        else{
+            //error
+        }
+        //pass return infromation to function which handles client interaction
+        sendToUser(sendBackData);
+    }
+     //send data back to client function
+    private void sendToUser(JSONAdapter sendData){
+        try{
+            //adds status header onto the JSON string, ready for sending to client
+            sendData.get(0).add(new JSONProperty("STATUS", String.valueOf(opSuccess), Tokenizer.TokenType.BOOLEAN ));
+            
+            //convert the JSON object into string , then bytes ready to be sent to client
+            String message = sendData.toString();
+            byte[] data = message.getBytes();
+            
+            //send data back to client
+            DatagramPacket dataToSend = new DatagramPacket(data, data.length, userIP, userPort);
+            socket.send(dataToSend);
+            
+        }catch(Exception e){Log.Throw(e);}
+    }
+    
+    //function to change error status if issue with performing an operation for the client
+    private void Error(boolean error){
+        opSuccess = error;
+    }
+    
+    //function to return messages to the client based on what has been sent to them
+    private ResultSet receiveMessages(){
+        return null;
+    }
+    
+    //function which allows the client to send messages to other users
+    private void sendMessage(JSONAdapter info){
         
     }
 }
